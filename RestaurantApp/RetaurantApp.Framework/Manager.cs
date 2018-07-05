9b5976212ec35cb90dc41b2c93d6942cadf26701
Viju Rajan <vijurajan001@gmail.com>
2018-07-05 18:38:02 +0530

    Addinf Project Initial Commit

---
+++ RestaurantApp/RetaurantApp.Framework/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetaurantApp.Framework
+{
+    public class Manager
+    {
+        
+
+        public void RegisterComplaints() { }
+        public void GetFeedBack() { }
+        public void ProcessBillPayment() { }
+    }
+}
---
+++ RestaurantApp/RetaurantApp.Framework/Retaurant.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RetaurantApp.Framework
+{
+    public class Class1
+    {
+    }
+}
